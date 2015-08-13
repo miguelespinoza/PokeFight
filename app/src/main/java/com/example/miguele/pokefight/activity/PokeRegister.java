2c1a077d80794e9e87768105c44cd345f802f793
Miguel Espinoza <miguelespinoza349@yahoo.com>
2015-08-12 20:14:37 -0400

    onto selecting challenger

--- app/src/main/java/com/example/miguele/pokefight/activity/PokeRegister.java
+++ app/src/main/java/com/example/miguele/pokefight/activity/PokeRegister.java
@@ -1,20 +1,32 @@
 package com.example.miguele.pokefight.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.miguele.pokefight.R;
+import com.example.miguele.pokefight.storage.SyncInfo;
+import com.firebase.client.Firebase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import jonathanfinerty.once.Once;
 
 public class PokeRegister extends ActionBarActivity {
+
+    private static final String TAG = "activity_poke_register";
+
+    private Firebase challengerFirebase = null;
 
     @Bind(R.id.toolbar) Toolbar mToolbar;
     @Bind(R.id.challenger_et) EditText mChallengerEditText;
@@ -25,10 +37,14 @@ public class PokeRegister extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         Once.initialise(this);
+
         String firstTimeRegister = "firstTimeRegister";
         if (!Once.beenDone(Once.THIS_APP_INSTALL, firstTimeRegister)) {
             setContentView(R.layout.activity_poke_register);
             ButterKnife.bind(this);
+
+            setupUI();
+            setupFirebase();
 
             // Onced registered successfully
             Once.markDone(firstTimeRegister);
@@ -65,13 +81,54 @@ public class PokeRegister extends ActionBarActivity {
         initializeToolbar();
     }
 
+    private void setupFirebase() {
+        Firebase.setAndroidContext(this);
+        challengerFirebase = new Firebase("https://poke-testing.firebaseio.com/");
+    }
+
     // Initialize toolbar
     public void initializeToolbar() {
         if (mToolbar != null) {
-            mToolbar.setTitle(R.string.app_name);
+            mToolbar.setTitle("Register");
             mToolbar.setBackgroundColor(getResources().getColor(R.color.primaryBlue500));
 
             setSupportActionBar(mToolbar);
         }
+    }
+
+    @OnClick(R.id.challenger_submit_btn)
+    public void submitName() {
+        Log.i(TAG, "submitName to Firebase");
+
+        String name = mChallengerEditText.getText().toString();
+        Firebase usersRef = challengerFirebase.child("users");
+        Firebase newUserRef = usersRef.push();
+
+        Map<String, String> user = new HashMap<>();
+        user.put("name", name);
+        user.put("status", "available");
+        newUserRef.setValue(user);
+
+        // Get the unique ID genereated by push()
+        String userID = newUserRef.getKey();
+
+        // Save this info for later...
+        SyncInfo.setYourInfo(this, name, userID);
+
+        // Intent to PokeFight select challenger
+        Intent challengerIntent = new Intent(PokeRegister.this, PokeChallenger.class);
+        PokeRegister.this.startActivity(challengerIntent);
+
+        // Testing to see what we get for reading child
+//        challengerFirebase.child("message").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//                System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError error) {
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/miguele/pokefight/storage/SyncInfo.java
+++ app/src/main/java/com/example/miguele/pokefight/storage/SyncInfo.java
@@ -10,11 +10,13 @@ public class SyncInfo {
 
     private static final String STORE_CHALLENGER_NAME = "com.example.miguele.pokefight.storage.challenger_name";
     private static final String STORE_YOUR_NAME = "com.example.miguele.pokefight.storage.your_name";
+    private static final String STORE_YOUR_ID = "com.example.miguele.pokefight.storage.your_id";
     private static final String STORE_POKE_RESOURCE = "com.example.miguele.pokefight.storage.poke_resource_uri";
     private static final String STORE_CHALLENGE_ID = "com.example.miguele.pokefight.storage.challenge_id";
 
     private static final String CHALLENGER_NAME = "challenger_name";
     private static final String YOUR_NAME = "your_name";
+    private static final String YOUR_ID = "your_id";
     private static final String POKE_RESOURCE = "poke_resource_uri";
     private static final String CHALLENGE_ID = "challenge_id";
 
@@ -31,12 +33,19 @@ public class SyncInfo {
 
     }
 
-    public static void setYourName(Context context, String name) {
+    public static void setYourInfo(Context context, String name, String id) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(
                 STORE_YOUR_NAME,
                 Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(YOUR_NAME, name);
+        editor.commit();
+
+        sharedPreferences = context.getSharedPreferences(
+                STORE_YOUR_ID,
+                Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        editor.putString(YOUR_NAME, id);
         editor.commit();
     }
 
@@ -56,6 +65,8 @@ public class SyncInfo {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(CHALLENGE_ID, id);
         editor.commit();
+
+
     }
 
     public static String getChallengerName(Context context) {
@@ -67,7 +78,7 @@ public class SyncInfo {
         return challengerName;
     }
 
-    public static String getYourName(Context) {
+    public static String getYourName(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(
                 STORE_YOUR_NAME,
                 Context.MODE_PRIVATE);
@@ -76,7 +87,16 @@ public class SyncInfo {
         return yourName;
     }
 
-    public static String getPokeResourceUri(Context) {
+    public static String getYourId(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(
+                STORE_YOUR_ID,
+                Context.MODE_PRIVATE);
+        String yourId = sharedPreferences.getString(YOUR_ID, "");
+
+        return yourId;
+    }
+
+    public static String getPokeResourceUri(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(
                 STORE_POKE_RESOURCE,
                 Context.MODE_PRIVATE);
